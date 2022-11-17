457ae2a14153340f7e1307833bbb31c0af8ec678
Jerry Liu <jiaruirz@gmail.com>
2022-11-17 02:27:57 -0500

    reinitialize db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Spot.create!([
-{"time2leave": '2022-11-20 20:51:14', "latitude": "40.809893", "longitude": "-73.960547"},
-{"time2leave": '2022-11-19 17:33:29', "latitude": "40.807346", "longitude": "-73.960714"},
-{"time2leave": '2022-12-16 00:51:14', "latitude": "40.808479", "longitude": "-73.963412"}
+{"time2leave": '2022-11-20 20:51:14', "going": '0', "latitude": "40.809893", "longitude": "-73.960547"},
+{"time2leave": '2022-11-19 17:33:29', "going": '0', "latitude": "40.807346", "longitude": "-73.960714"},
+{"time2leave": '2022-12-16 00:51:14', "going": '0', "latitude": "40.808479", "longitude": "-73.963412"}
 ])
